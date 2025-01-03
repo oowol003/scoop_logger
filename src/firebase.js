c2a7a49966ae7ac0458e8c6b5359be7f9212b9bb
damolaowolabi <114254042+damolaowolabi@users.noreply.github.com>
2025-01-02 21:32:13 -0600

    Update Firebase configuration and add Firestore rules

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAfjf4aJG9WnHrr0KbTyMX-UJYA658XHks",
+  authDomain: "scoop-logger.firebaseapp.com",
+  projectId: "scoop-logger",
+  storageBucket: "scoop-logger.firebasestorage.app",
+  messagingSenderId: "503931309386",
+  appId: "1:503931309386:web:893ef061c68749dd5c85b3",
+  measurementId: "G-S8V2L95E0X"
 };
 
 // Initialize Firebase
